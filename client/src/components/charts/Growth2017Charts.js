86d01f8b8d31a60d93f9e29b908e393bc0581959
Eric-Lingren <44101756+Eric-Lingren@users.noreply.github.com>
2019-02-10 19:32:29 -0700

    added labels to cursor on 2017 growth chart.  Cleaned up code

--- client/src/components/charts/Growth2017Charts.js
+++ client/src/components/charts/Growth2017Charts.js
@@ -24,79 +24,69 @@ class Growth2017Chart extends Component {
 
     draw2017GrowthChart = () => {
         let my2017GrowthData = this.state.daily2017Growth
-        
-        // console.log('chart rendered')
         const node = this.node
-        // let width = window.innerWidth;
-        // let height = window.innerHeight - 50;
 
-
-        // 2. Use the margin convention practice 
-        var margin = {top: 20, right: 50, bottom: 50, left: 200}
-        , width = ((window.innerWidth - 50) - margin.left - margin.right) // Use the window's width 
-        , height = ((window.innerHeight  / 2) - 50); // Use the window's height
+        // Use the margin convention practice 
+        var margin =    {top: 20, right: 0, bottom: 50, left: 200}
+                        , width = ((window.innerWidth - 120) - margin.left) 
+                        , height = ((window.innerHeight  / 2) - 50);    
 
         // The number of datapoints
         let n = my2017GrowthData.length
 
-        let bisectDate = d3.bisector(function(d) { return d; }).left;
-
         var x = d3.scaleLinear().range([0, width]);
         var y = d3.scaleLinear().range([height, 0]);
 
-        // 5. X scale will use the index of our data
+        // X scale will use the index of our data
         var xScale = d3.scaleLinear()
-            .domain([0, n-1]) // input
-            .range([0, width]); // output
+            .domain([0, n-1])                           // input
+            .range([0, width]);                         // output
 
-        // 6. Y scale  
+        // Y scale  
         var yScale = d3.scaleLinear()
-            .domain([0, 10]) // input 
-            .range([height, 0]); // output
+            .domain([0, 10])                            // input 
+            .range([height, 0]);                        // output
 
-        // 7. d3's line generator
+        // d3's line generator
         var line = d3.line()
-            .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-            .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-            .curve(d3.curveMonotoneX) // apply smoothing to the line
+            .x(function(d, i) { return xScale(i); })    // set the x values for the line generator
+            .y(function(d) { return yScale(d.y); })     // set the y values for the line generator 
+            .curve(d3.curveMonotoneX)                   // apply smoothing to the line
 
         var dataset = my2017GrowthData
 
-        // 1. Add the chart to the page via the SVG node and employ #2
-        //var svg = d3.select("body").append("svg")
+        // Add the chart to the page via the SVG node
         let chart = d3.select(node)
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
         .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        
-
-
-        // 3. Call the x axis in a group tag
+        // Call the x axis in a group tag
         chart.append("g")
             .attr("class", "x axis")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+            .call(d3.axisBottom(xScale));                       // Create an axis component with d3.axisBottom
 
-        // 4. Call the y axis in a group tag
+        // Call the y axis in a group tag
         chart.append("g")
             .attr("class", "y axis")
-            .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+            .call(d3.axisLeft(yScale));                         // Create an axis component with d3.axisLeft
 
-        // 9. Append the path, bind the data, and call the line generator 
+        // Append the path, bind the data, and call the line generator 
         chart.append("path")
-            .datum(dataset) // 10. Binds data to the line 
-            .attr("class", "line") // Assign a class for styling 
-            .attr("d", line); // 11. Calls the line generator 
-        
-        chart.append("text")      // text label for the x axis
+            .datum(dataset)                                     // Binds data to the line 
+            .attr("class", "line")                              // Assign a class for styling 
+            .attr("d", line);                                   // Calls the line generator 
+
+        // text label for the x axis
+        chart.append("text")
             .attr("x", (window.innerWidth / 2 - 150 ) )
             .attr("y", (window.innerHeight / 2 - 10 ) )
             .attr("font-size", "18px")
             .attr("font-weight", "bold")
             .style("text-anchor", "middle")
-            .text("Trades Taken");
+            .text("Quantity of Trades Taken");
 
         chart.append("text")
             .attr("transform", "rotate(-90)")
@@ -106,48 +96,45 @@ class Growth2017Chart extends Component {
             .attr("font-size", "18px")
             .attr("font-weight", "bold")
             .style("text-anchor", "middle")
-            .text("Growth in %");
+            .text("Account Percent Growth");
 
-            ////////////////////////////////////////////////////
-        var focus = chart.append("g")                                // **********
+        var focus = chart.append("g")
             .style("display", "none");   
 
-            // append the circle at the intersection               // **********
-        focus.append("circle")                                 // **********
-            .attr("class", "y")                                // **********
-            .style("fill", "blue")                             // **********
-            .style("stroke", "blue")                           // **********
-            .attr("r", 4);   
-            
-        
+        // Append the circle at the intersection
+        focus.append("circle")
+            .attr("class", "y")
+            .style("fill", "blue")
+            .style("stroke", "blue")
+            .attr("r", 5);   
 
-            // append the rectangle to capture mouse               // **********
-        chart.append("rect")                                     // **********
-            .attr("width", width)                              // **********
-            .attr("height", height)                            // **********
-            .style("fill", "none")                             // **********
-            .style("pointer-events", "all")                    // **********
+        // Append the rectangle to capture mouse
+        chart.append("rect")
+            .attr("width", width)
+            .attr("height", height)
+            .style("fill", "none")
+            .style("pointer-events", "all")
             .on("mouseover", function() { focus.style("display", null); })
             .on("mouseout", function() { focus.style("display", "none"); })
-            .on("mousemove", mousemove);                       // **********
+            .on("mousemove", mousemove);
 
         function mousemove() {   
+            focus.select("text").remove()
             var mouse = d3.mouse(this);
             var mouseDate = xScale.invert(mouse[0]);
-            var mouseY = yScale.invert(mouse[1]);
             let mouseTrade = Math.floor(mouseDate);
 
-            // var y0 = y.invert(d3.mouse(this)[1]);
-
             var x0 = x.invert(d3.mouse(this)[0]),  
-            
             i = findIndexValue(dataset, mouseTrade )            
-            
+            let yScalePoint;
+
             function findIndexValue(d, m){
-                let yScalePoint = d[m].y
+                yScalePoint = d[m].y
                 let moveDecimal = yScalePoint/10
                 return moveDecimal
             }
+
+            yScalePoint = Math.round(yScalePoint * 100) / 100
 
             focus.select("circle.y")                           
                 .attr("transform",                             
@@ -155,12 +142,12 @@ class Growth2017Chart extends Component {
                     
             focus.append("text")
                 .attr("transform",                             
-                "translate(" + x(x0) + "," + y(i+.1) + ")")
+                "translate(" + x(x0 - .08) + "," + y(i+.15) + ")")
                     .attr("dy", "1em")
-                    //.attr('id',  mouseTrade)
-                    .attr("font-size", "12px")
-                    .attr("font-weight", "bold")
-                    .text("Growth in %");
+                    .attr("font-size", "14px")
+                    //.attr("font-weight", "bold")
+                    .attr('class', 'chart-text')
+                    .text(`Trade #: ${mouseTrade} - Growth: ${yScalePoint}%`);
         }
     }
 
